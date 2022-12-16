ff1d64264247e864b0bda06256d770994295f33a
thomasstrassmann <thomas_strassmann@freenet.de>
2022-12-16 13:55:10 +0000

    add serializer

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import os
 
 if os.path.exists('env.py'):
     import env
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'rest_framework',
     'profiles',
 ]
 
